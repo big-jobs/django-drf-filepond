545cf5de6bfd5c8d0378518d7fcc5237ba759466
Ujjwal Tak <24545193+codefoxut@users.noreply.github.com>
2026-04-15 14:32:02 +0530

    fix tests: pass thumbnail_type=None to get_stored_upload_file_data
    Update test_api_getupload.py to pass the new required thumbnail_type
    argument (None) to get_stored_upload_file_data, fixing state pollution
    that caused cascading failures in test_api_remote and test_api.
    
    Also update docs version to 0.5.2.1.

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019-2025, Jeremy Cohen, Imperial College London and contributors'
 author = 'Jeremy Cohen and contributors'
 
 # The short X.Y version
-version = '0.5.1'
+version = '0.5.2.1'
 # The full version, including alpha/beta/rc tags
-release = '0.5.1'
+release = '0.5.2.1'
 
 
 # -- General configuration ---------------------------------------------------
